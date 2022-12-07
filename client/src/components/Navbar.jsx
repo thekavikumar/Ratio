f6a4fc67f7dbf13308f8a0e8df33b0146e199374
Kavikumar M <kavikumarceo@gmail.com>
2022-12-07 22:35:41 +0530

    title and logo update

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { HiMenuAlt4 } from "react-icons/hi";
 import { AiOutlineClose } from "react-icons/ai";
-import logo from "../../images/logo.png";
+import logo from "../../images/main.png";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const NavBarItem = ({ title, classprops }) => (
@@ -13,9 +13,13 @@ const Navbar = () => {
 
   const { loginWithPopup, logout, user, isAuthenticated } = useAuth0();
   return (
-    <nav className="w-full flex md:justify-center justify-between items-center p-4">
-      <div className="md:flex-[0.5] flex-initial justify-center items-center">
-        <img src={logo} alt="logo" className="w-32 cursor-pointer" />
+    <nav className="w-full flex md:justify-center justify-between items-center p-5">
+      <div className="md:flex-[0.7] flex-initial justify-center items-center">
+        <div className="flex cursor-pointer items-center">
+          <img src={logo} alt="logo" className="logo" />
+          <h1 className="text-white text-3xl ">Ratio</h1>
+        </div>
+        {/* <img src={logo} alt="logo" className="w-32 cursor-pointer" /> */}
       </div>
       <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
         {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
