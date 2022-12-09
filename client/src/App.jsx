99a4c8a3c26faaf4417f4d3fbdbf28169dfd759c
Kavikumar M <kavikumarceo@gmail.com>
2022-12-09 13:09:53 +0530

    navbar update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,25 @@
-import { Navbar, Welcome, Footer, Services, Transactions } from "./components";
-import iphone from "../images/Composition.png";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Generate from "./components/Generate";
+import Order from "./components/Order";
+import Home from "./Home";
 const App = () => (
-  <div className="min-h-screen gradient-bg">
-    <div className="">
-      <Navbar />
-      <Welcome />
-      <img src={iphone} alt="iphone" className="iphone" />
-    </div>
-    <Services />
-    <Transactions />
-    <Footer />
-  </div>
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/generate" element={<Generate />} />
+      <Route path="/order" element={<Order />} />
+      {/* <div className="min-h-screen gradient-bg">
+        <div className="">
+          <Navbar />
+          <Welcome />
+          <img src={iphone} alt="iphone" className="iphone" />
+        </div>
+        <Services />
+        <Transactions />
+        <Footer />
+      </div> */}
+    </Routes>
+  </Router>
 );
 
 export default App;
