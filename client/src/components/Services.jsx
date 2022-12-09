4ee187602b250343440addc3cac6b418ef6ce00f
P Gopikishaan <kishaangopi@gmail.com>
2022-12-09 17:22:20 +0530

    services section

--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -2,16 +2,17 @@ import React from "react";
 import { BsShieldFillCheck } from "react-icons/bs";
 import { BiSearchAlt } from "react-icons/bi";
 import { RiHeart2Fill } from "react-icons/ri";
+import hydera from "../../images/hydera.jpeg";
 
-const ServiceCard = ({ color, title, icon, subtitle }) => (
-  <div className="flex flex-row justify-start items-start white-glassmorphism p-3 m-2 cursor-pointer hover:shadow-xl">
+const ServiceCard = ({ title, image, subtitle }) => (
+  <div className="black-glassmorphism px-1 py-8 m-2 cursor-pointer">
     <div
-      className={`w-10 h-10 rounded-full flex justify-center items-center ${color}`}
+      className={`ml-5 w-14 h-14 rounded-lg flex justify-center items-center bg-white`}
     >
-      {icon}
+      <img className="h-12 w-12" src={image} />
     </div>
-    <div className="ml-5 flex flex-col flex-1">
-      <h3 className="mt-2 text-white text-lg">{title}</h3>
+    <div className="ml-5">
+      <h3 className="mt-2 text-white text-xl mb-2">{title}</h3>
       <p className="mt-1 text-white text-sm md:w-9/12">{subtitle}</p>
     </div>
   </div>
@@ -19,37 +20,28 @@ const ServiceCard = ({ color, title, icon, subtitle }) => (
 
 const Services = () => (
   <div className="flex w-full justify-center items-center">
-    <div className="flex mf:flex-row flex-col items-center justify-between md:p-20 py-12 px-4">
-      <div className="flex-1 flex flex-col justify-start items-start">
-        <h1 className="text-white text-3xl sm:text-5xl py-2 text-gradient ">
-          Services that we
-          <br />
-          continue to improve
+    <div className=" mf:flex-row flex-col items-center justify-between md:p-20 py-12 px-4">
+      <div className="">
+        <h1 className="text-white text-lg sm:text-3xl py-4">
+          Services that we known for
         </h1>
-        <p className="text-left my-2 text-white font-light md:w-9/12 w-11/12 text-base">
-          The best choice for buying and selling your crypto assets, with the
-          various super friendly services we offer
-        </p>
       </div>
 
-      <div className="flex-1 flex flex-col justify-start items-center">
+      <div className="flex-1 flex justify-start items-center">
         <ServiceCard
-          color="bg-[#2952E3]"
-          title="Security gurantee"
-          icon={<BsShieldFillCheck fontSize={21} className="text-white" />}
-          subtitle="Security is guranteed. We always maintain privacy and maintain the quality of our products"
+          title="Token Service"
+          image={hydera}
+          subtitle="Mint and manage fungible and non-fungible token on Ratio for application payments, governance, or digital collectibles."
         />
         <ServiceCard
-          color="bg-[#8945F8]"
-          title="Best exchange rates"
-          icon={<BiSearchAlt fontSize={21} className="text-white" />}
-          subtitle="Security is guranteed. We always maintain privacy and maintain the quality of our products"
+          title="File Service"
+          image="https://media.istockphoto.com/id/1175215972/vector/file-folder-in-flat-on-white-background.jpg?s=612x612&w=0&k=20&c=dALCaVx9KdXJkgPO6Bjim_3TLZA9rnN__9gZRdo1zQ8="
+          subtitle="Store smart contract byte code, address books, credentials, and other critical files on the Hydera network."
         />
         <ServiceCard
-          color="bg-[#F84550]"
-          title="Fastest transactions"
-          icon={<RiHeart2Fill fontSize={21} className="text-white" />}
-          subtitle="Security is guranteed. We always maintain privacy and maintain the quality of our products"
+          title="Network Service"
+          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsOxJrhE6zKBgRQKdqSOCJ3eiNgU0ez00Ks_VhpkzXwPbElAfg9Rpdx2CMZpMTPRdlRbU&usqp=CAU"
+          subtitle="Use the Hydera API to interact with high speed services with low fees that utilize the efficiency of hashgraph."
         />
       </div>
     </div>
