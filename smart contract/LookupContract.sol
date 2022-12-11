399e2f0c73b2a332b1ba16bc89a65f7cd71cef93
Kavikumar M <kavikumarceo@gmail.com>
2022-12-11 11:27:00 +0530

    smart

--- smart contract/LookupContract.sol
+++ smart contract/LookupContract.sol
@@ -1,12 +1,21 @@
-//SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
+ 
 
 contract LookupContract {
-    mapping(string => uint256) public myDirectory;
 
-    constructor(string memory _name, uint256 _mobileNumber) public {
+mapping (string => uint) public myDirectory;
+
+constructor (string memory _name, uint _mobileNumber) public {
         myDirectory[_name] = _mobileNumber;
     }
-    function set
-}
 
+function setMobileNumber(string memory _name, uint _mobileNumber) public {
+        myDirectory[_name] = _mobileNumber;
+    }
+
+function getMobileNumber(string memory _name) public view returns (uint) {
+        return myDirectory[_name];
+    }
+
+}
